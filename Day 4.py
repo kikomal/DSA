a2e5991294209100578e9a75604d9c6d66dd2b95
kikomal <88785583+kikomal@users.noreply.github.com>
2021-08-13 22:31:18 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,26 @@
+def search(num, n, a):
+    for i in range(n):
+        if a[i]==num:
+            pos=i
+            print("Given value is found at", pos+1)
+            break
+        else:
+            print("Number not found")
+            break
+        
+
+
+
+
+
+
+n=int(input("Enter the size of array: "))
+a=[]
+for i in range(n):
+    val=int(input("Enter the value in array: "))
+    a.append(val)
+num=int(input("Enter the value that you want to search: "))
+
+
+search(num, n, a)
+    
